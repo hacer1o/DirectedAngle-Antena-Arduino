c1da2eb043c972b22d3183ed04b31643c408a7b7
vbodnarchuk <63015759+vbodnarchuk@users.noreply.github.com>
2020-06-21 22:04:06 +0300

    Add files via upload

---
+++ __________.ino
@@ -0,0 +1,36 @@
+int r_in;
+int radius_stats;
+int b1_in;
+int b2_in;
+int max_correct = 1023;
+int min_correct = 0;
+#include <LiquidCrystal.h>
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+void setup() {
+pinMode(A0 ,INPUT);
+pinMode(6 ,INPUT);
+pinMode(7 ,INPUT);
+ lcd.begin(16, 2);
+ Serial.begin(9600);
+}
+
+void loop() {
+  r_in = analogRead(0);
+  radius_stats = map(r_in , min_correct , max_correct , 0 , 360);
+  lcd.setCursor(0, 0);
+  lcd.print(radius_stats);
+  Serial.println(radius_stats);
+  lcd.clear();
+  b1_in = digitalRead(6);
+  b2_in = digitalRead(7);
+  if(b1_in){
+    min_correct = r_in;
+  }
+  Serial.println(min_correct);
+  if(b2_in){
+    max_correct = r_in;
+  }
+  Serial.println(max_correct);
+  delay(500);
+}
